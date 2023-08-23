a7f46bf06b81d6329567447094996c721c8b50d1
Mert Sekü <mrtseku@gmail.com>
2023-08-24 01:51:22 +0300

    Todo project

---
+++ src/app/model.ts
@@ -0,0 +1,24 @@
+export class Model {
+    user;
+    items;
+
+    constructor(){
+        this.user = "Sekü";
+        this.items= [
+            new TodoItem("Spor",true),
+            new TodoItem("Kahvaltı",false),
+            new TodoItem("Kitap Okumak",false),
+            new TodoItem("Sinema",false),
+          ]
+    }
+}
+
+export class TodoItem{
+    description;
+    action;
+
+    constructor(description: any, action: any){
+        this.description = description;
+        this.action = action;
+    }
+}
